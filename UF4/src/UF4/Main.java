58e119c67028df3433b59e3ff26143a34dbaf654
Rafael.F <rafa2597@hotmail.com>
2024-05-30 20:32:42 +0200

    Coloritos

--- UF4/src/UF4/Main.java
+++ UF4/src/UF4/Main.java
@@ -3,10 +3,15 @@ package UF4;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		        System.out.println("Hola Mauri");
-		        System.out.println("¿Que tal?");
-
+		
+		        String rojo = "\u001B[31m";
+		        String naranja = "\u001B[38;5;208m";
+		        String amarillo = "\u001B[33m";
+		        String verde = "\u001B[32m";
+		        String azul = "\u001B[36m";
+		        String morado = "\u001B[35m";
+		        String reset = "\u001B[0m"; 
+		        
+		        System.out.println(rojo + "F" + naranja + "e" + amarillo + "l" + verde + "i" + azul + "z" + morado + " " + rojo + "J" + naranja + "u" + amarillo + "n" + verde + "i" + azul + "o" + reset);
 	}
-
 }
