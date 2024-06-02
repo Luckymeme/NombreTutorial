7be8aa5eb2b3a4f3bb894afeca5f246a47db562a
Rafael.F <rafa2597@hotmail.com>
2024-06-02 21:48:13 +0200

    Coloritos
    Añadida la función coloritos al main

--- UF4/src/UF4/Main.java
+++ UF4/src/UF4/Main.java
@@ -7,6 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);
+		
+		String rojo = "\u001B[31m";
+        String naranja = "\u001B[38;5;208m";
+        String amarillo = "\u001B[33m";
+        String verde = "\u001B[32m";
+        String azul = "\u001B[36m";
+        String morado = "\u001B[35m";
+        String reset = "\u001B[0m"; 
+        
+        System.out.println(rojo + "F" + naranja + "e" + amarillo + "l" + verde + "i" + azul + "z" + morado + " " + rojo + "J" + naranja + "u" + amarillo + "n" + verde + "i" + azul + "o" + reset);
 
 		System.out.println("Inserta el texto que deseas repetir:");
 		String texto = teclado.nextLine();
